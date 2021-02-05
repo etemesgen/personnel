e285d9360372ef5f2d11afe42c62035f5eed0c4b
Edomiyas Temesgen <temesgen.edomiyas@yahoo.com>
2021-02-05 14:51:48 +0100

    test-unitaires commit

--- Personnel/src/testsUnitaires/testLigue.java
+++ Personnel/src/testsUnitaires/testLigue.java
@@ -52,25 +52,15 @@ class testLigue
 		      Ligue ligue = gestionPersonnel.addLigue("ligueOne");
 	          assertEquals("ligueOne", ligue.toString());
 	   }
-<<<<<<< HEAD
-	   
-=======
 
-	   @Test
->>>>>>> branch 'master' of https://github.com/etemesgen/personnel.git
 	   void compareTo() throws SauvegardeImpossible{
 		      Ligue ligue = gestionPersonnel.addLigue("ligueOne");
 		      Ligue ligueTwo = gestionPersonnel.addLigue("ligueTwo");
 	          assertTrue(ligueTwo.compareTo(ligue)>0);
 	   }
-	   
-<<<<<<< HEAD
-	   void getAdmin() throws SauvegardeImpossible{
-=======
 
        @Test
        void getAdmin() throws SauvegardeImpossible{
->>>>>>> branch 'master' of https://github.com/etemesgen/personnel.git
 		      Ligue ligue = gestionPersonnel.addLigue("ligueOne");
 		      assertEquals(gestionPersonnel.getRoot(), ligue.getAdministrateur());
 	   }
