065c2eb809c57dc890f4ce3c086e0bf8d97690f8
diariyata <diariyata@hotmail.com>
2021-02-12 15:07:53 +0100

    C:/Users/Diari/script bdd.sql

--- script bdd.sql
+++ script bdd.sql
@@ -1,12 +1,7 @@
 -- -----------------------------------------------------------------------------
---             Génération d'une base de données pour
---                           MySQL
---                     (20/1/2021 13:00:00)
--- -----------------------------------------------------------------------------
 --      Nom de la base : GestionPersonnel
 --      Projet : PPE application JAVA
 --      Auteur : Diariyata & Edomiyas
---      Date de dernière modification : 
 -- -----------------------------------------------------------------------------
 
 -- -----------------------------------------------------------------------------
@@ -22,12 +17,12 @@ GestionPersonnel;
 
 CREATE TABLE Employe
    (
-    id_employe INT(11) NOT NULL AUTO_INCREMENT,   
-    nom_employe VARCHAR(40) NULL  ,
-	prenom_employe VARCHAR(50) NULL  ,
-	mail VARCHAR(100) NULL  ,
-	password VARCHAR(11) NULL  , 
-	niveau_privilège VARCHAR(25) NULL  ,
+    id_employe INT(11) AUTO_INCREMENT,   
+    nom_employe VARCHAR(40) NOT NULL  ,
+	prenom_employe VARCHAR(50) NOT NULL  ,
+	mail VARCHAR(100) NOT NULL  ,
+	password VARCHAR(11) NOT NULL  , 
+	niveau_privilège INT(11) NULL  ,
 	date_arrivee DATE NOT NULL  ,
 	date_depart DATE NULL ,
 	 PRIMARY KEY (id_employe) 
@@ -39,7 +34,9 @@ CREATE TABLE Employe
 
 CREATE TABLE Ligue
  (
-   num_ligue INT(11) NOT NULL AUTO_INCREMENT, 
-   nom_ligue VARCHAR(35) NULL  ,
+   num_ligue INT(11) AUTO_INCREMENT, 
+   nom_ligue VARCHAR(35) NOT NULL  ,
 	PRIMARY KEY (num_ligue) 
  );
+ 
+ foreign key('num_ligue') references Ligue('num_ligue');
