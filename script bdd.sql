f9026e4e68afe48cce53a8615fdba170dd8e5c37
diariyata <diariyata@hotmail.com>
2021-03-09 15:53:06 +0100

    update

--- script bdd.sql
+++ script bdd.sql
@@ -1,12 +1,7 @@
 -- -----------------------------------------------------------------------------
---             Génération d'une base de données pour
---                           MySQL
---                     (20/1/2021 13:00:00)
--- -----------------------------------------------------------------------------
 --      Nom de la base : GestionPersonnel
 --      Projet : PPE application JAVA
 --      Auteur : Diariyata & Edomiyas
---      Date de dernière modification : 
 -- -----------------------------------------------------------------------------
 
 -- -----------------------------------------------------------------------------
@@ -17,29 +12,32 @@ CREATE DATABASE
 GestionPersonnel;
 
 -- -----------------------------------------------------------------------------
---       TABLE : Employe
--- -----------------------------------------------------------------------------
-
-CREATE TABLE Employe
-   (
-    id_employe INT(11) NOT NULL AUTO_INCREMENT,   
-    nom_employe VARCHAR(40) NULL  ,
-	prenom_employe VARCHAR(50) NULL  ,
-	mail VARCHAR(100) NULL  ,
-	password VARCHAR(11) NULL  , 
-	niveau_privilège VARCHAR(25) NULL  ,
-	date_arrivee DATE NOT NULL  ,
-	date_depart DATE NULL ,
-	 PRIMARY KEY (id_employe) 
-   );
-   
--- -----------------------------------------------------------------------------
 --       TABLE : Ligue
 -- -----------------------------------------------------------------------------
 
 CREATE TABLE Ligue
  (
-   num_ligue INT(11) NOT NULL AUTO_INCREMENT, 
-   nom_ligue VARCHAR(35) NULL  ,
+   num_ligue INT(11) AUTO_INCREMENT, 
+   nom_ligue VARCHAR(35) NOT NULL ,
 	PRIMARY KEY (num_ligue) 
  );
+ 
+-- -----------------------------------------------------------------------------
+--       TABLE : Employe
+-- -----------------------------------------------------------------------------
+
+CREATE TABLE Employe
+   (
+    id_employe INT(11) AUTO_INCREMENT,   
+    nom_employe VARCHAR(40) NOT NULL  ,
+	prenom_employe VARCHAR(50) NOT NULL  ,
+	mail VARCHAR(100) NOT NULL  ,
+	password VARCHAR(11) NOT NULL  , 
+	niveau_privilege INT(11) NULL  ,
+	date_arrivee DATE NOT NULL  ,
+	date_depart DATE NULL ,
+	num_ligue INT(11) NULL ,
+	 PRIMARY KEY (id_employe) ,
+	 constraint fk_employe_ligue foreign key (num_ligue) references Ligue (num_ligue)
+   );
+   
