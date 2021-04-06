35db535ace77e1f97dea3d84f545cd87ed75bfa4
Edomiyas Temesgen <temesgen.edomiyas@yahoo.com>
2021-04-06 18:13:16 +0200

    Accueil page presque terminé bouton a bien modéliser.

--- Personnel/src/interfaceGraphique/Accueil.java
+++ Personnel/src/interfaceGraphique/Accueil.java
@@ -1,33 +1,31 @@
 package interfaceGraphique;
 
+import java.awt.Color;
+import java.awt.Font;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JPanel;
 
 public class Accueil {
 	public static void main(String[] args)
 	{
+		JLabel l = new JLabel("GESTION DU PERSONNEL DES LIGUES", JLabel.CENTER); //créer le texte
+		l.setFont(new Font("Rockwell Nova", Font.PLAIN, 38));
+		
 		JFrame f = new JFrame(); //créer une instance de JFrame  
 		f.setTitle("GESTION DU PERSONNEL DES LIGUES");
-		f.setSize(200, 200);
+		f.setSize(800, 500);
+		f.getContentPane().setBackground(Color.YELLOW);
+		f.add(l); // ajouter label à frame
 		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		f.setVisible(true);//rendre le frame visible
         
 		JButton b = new JButton("Suivant");//créer une instance de JButton  
 		b.setBounds(600,380,100, 40);//x axes, y axes, largeur, hauteur  
-		          
+		b.setBackground(Color.BLACK);   
+		b.setForeground(Color.WHITE);
+		//b.setBorder();
 		f.add(b);//ajouter bouton dans JFrame  
-		          
-		f.setSize(800,500);//400 largeur et 500 hauteur  
-		f.setLayout(null);//using no layout managers  
-		f.setVisible(true);//rendre le frame visible  
-		
-		JLabel l = new JLabel(); //créer le texte
-        l.setText("GESTION DU PERSONNEL DES LIGUES"); // ajouter du texte à label
-     
-        JPanel p = new JPanel(); // créer un panel
-        
-        p.add(l); // ajouter label à panel
-        f.add(p); // ajouter panel à frame
 	}
 }
