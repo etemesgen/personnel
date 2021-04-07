8ddb4d1039188c38c4ec7b9f0aaf81c7964ea184
Edomiyas Temesgen <temesgen.edomiyas@yahoo.com>
2021-04-07 18:09:29 +0200

    Interface graphique page d'accueil fait, page de connexion commencé.

--- Personnel/src/interfaceGraphique/Accueil.java
+++ Personnel/src/interfaceGraphique/Accueil.java
@@ -10,22 +10,22 @@ import javax.swing.JLabel;
 public class Accueil {
 	public static void main(String[] args)
 	{
-		JLabel l = new JLabel("GESTION DU PERSONNEL DES LIGUES", JLabel.CENTER); //créer le texte
+		JLabel l = new JLabel("GESTION DU PERSONNEL DES LIGUES", JLabel.CENTER); //Créer le texte et le centrer
 		l.setFont(new Font("Rockwell Nova", Font.PLAIN, 38));
 		
-		JFrame f = new JFrame(); //créer une instance de JFrame  
-		f.setTitle("GESTION DU PERSONNEL DES LIGUES");
+		JFrame f = new JFrame(); //Créer une instance de JFrame  
+		f.setTitle("Accueil");
 		f.setSize(800, 500);
 		f.getContentPane().setBackground(Color.YELLOW);
-		f.add(l); // ajouter label à frame
+		f.add(l); //Ajouter label à frame
 		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		f.setVisible(true);//rendre le frame visible
+		f.setVisible(true);//Rendre le frame visible
         
 		JButton b = new JButton("Suivant");//créer une instance de JButton  
 		b.setBounds(600,380,100, 40);//x axes, y axes, largeur, hauteur  
-		b.setBackground(Color.BLACK);   
+		b.setBackground(Color.BLACK); //Définir la couleur de fond  
 		b.setForeground(Color.WHITE);
-		//b.setBorder();
-		f.add(b);//ajouter bouton dans JFrame  
+		b.setFont(new Font("Rockwell Nova", Font.PLAIN, 15));
+		f.add(b);//Ajouter bouton dans JFrame  
 	}
 }
--- Personnel/src/interfaceGraphique/PageConnexion.java
+++ Personnel/src/interfaceGraphique/PageConnexion.java
@@ -1,5 +1,37 @@
 package interfaceGraphique;
 
-public class PageConnexion {
+import java.awt.Color;
+import java.awt.Font;
 
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.border.Border;
+import javax.swing.border.CompoundBorder;
+import javax.swing.border.EmptyBorder;
+
+public class PageConnexion {
+	public static void main(String[] args)
+	{
+		JLabel l = new JLabel("Identifiez-vous", JLabel.CENTER); //Créer le texte et le centrer
+		l.setFont(new Font("Rockwell Nova", Font.PLAIN, 35));
+	/*	Border border = l.getBorder();
+		Border margin = new EmptyBorder(10,10,10,10);
+		l.setBorder(new CompoundBorder(border, margin));*/
+		
+		JFrame f = new JFrame(); //Créer une instance de JFrame  
+		f.setTitle("Page de connexion");
+		f.setSize(800, 500);
+		f.getContentPane().setBackground(Color.YELLOW);
+		f.add(l); //Ajouter label à frame
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		f.setVisible(true);//Rendre le frame visible
+        
+		JButton b = new JButton("Se connecter");//créer une instance de JButton  
+		b.setBounds(300,300, 200, 70);//x axes, y axes, largeur, hauteur  
+		b.setBackground(Color.BLACK); //Définir la couleur de fond  
+		b.setForeground(Color.WHITE);
+		b.setFont(new Font("Rockwell Nova", Font.PLAIN, 15));
+		f.add(b);//Ajouter bouton dans JFrame  
+	}
 }
