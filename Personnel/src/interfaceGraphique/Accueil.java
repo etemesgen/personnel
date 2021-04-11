cbc629f47e45a0ad872363fce547df9da6cac0b1
Edomiyas Temesgen <temesgen.edomiyas@yahoo.com>
2021-04-11 18:28:03 +0200

    Page de connexion presque entamé reste à configurer la couleur de fond et continuer les autres pages !

--- Personnel/src/interfaceGraphique/Accueil.java
+++ Personnel/src/interfaceGraphique/Accueil.java
@@ -10,22 +10,22 @@ import javax.swing.JLabel;
 public class Accueil {
 	public static void main(String[] args)
 	{
-		JLabel l = new JLabel("GESTION DU PERSONNEL DES LIGUES", JLabel.CENTER); //Créer le texte et le centrer
-		l.setFont(new Font("Rockwell Nova", Font.PLAIN, 38));
+		JLabel label = new JLabel("GESTION DU PERSONNEL DES LIGUES", JLabel.CENTER); //Créer le texte et le centrer
+		label.setFont(new Font("Rockwell Nova", Font.PLAIN, 38));
 		
-		JFrame f = new JFrame(); //Créer une instance de JFrame  
-		f.setTitle("Accueil");
-		f.setSize(800, 500);
-		f.getContentPane().setBackground(Color.YELLOW);
-		f.add(l); //Ajouter label à frame
-		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		f.setVisible(true);//Rendre le frame visible
+		JFrame frame = new JFrame(); //Créer une instance de JFrame  
+		frame.setTitle("Accueil");
+		frame.setSize(800, 500);
+		frame.getContentPane().setBackground(Color.YELLOW);
+		frame.add(label); //Ajouter label à frame
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);//Rendre le frame visible
         
-		JButton b = new JButton("Suivant");//créer une instance de JButton  
-		b.setBounds(600,380,100, 40);//x axes, y axes, largeur, hauteur  
-		b.setBackground(Color.BLACK); //Définir la couleur de fond  
-		b.setForeground(Color.WHITE);
-		b.setFont(new Font("Rockwell Nova", Font.PLAIN, 15));
-		f.add(b);//Ajouter bouton dans JFrame  
+		JButton button = new JButton("Suivant");//créer une instance de JButton  
+		button.setBounds(600,380,100, 40);//x axes, y axes, largeur, hauteur  
+		button.setBackground(Color.BLACK); //Définir la couleur de fond  
+		button.setForeground(Color.WHITE);
+		button.setFont(new Font("Rockwell Nova", Font.PLAIN, 15));
+		frame.add(button);//Ajouter bouton dans JFrame  
 	}
 }
--- Personnel/src/interfaceGraphique/PageConnexion.java
+++ Personnel/src/interfaceGraphique/PageConnexion.java
@@ -2,29 +2,98 @@ package interfaceGraphique;
 
 import java.awt.Color;
 import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 public class PageConnexion {
 	public static void main(String[] args)
 	{
-		JLabel l = new JLabel("Identifiez-vous", JLabel.CENTER); //Créer le texte et le centrer
-		l.setFont(new Font("Rockwell Nova", Font.PLAIN, 35));
+		JLabel label = new JLabel("Identifiez-vous", JLabel.CENTER); //Créer le texte et le centrer
+		label.setFont(new Font("Rockwell Nova", Font.PLAIN, 35));
+		label.setVerticalAlignment(JLabel.TOP);
 		
-		JFrame f = new JFrame(); //Créer une instance de JFrame  
-		f.setTitle("Page de connexion");
-		f.setSize(800, 500);
-		f.getContentPane().setBackground(Color.YELLOW);
-		f.add(l); //Ajouter label à frame
-		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		f.setVisible(true);//Rendre le frame visible
+		
+		JFrame frame = new JFrame(); //Créer une instance de JFrame  
+		frame.setTitle("Page de connexion");
+		frame.setSize(800, 500);
+		frame.getContentPane().setBackground(Color.YELLOW);
+		frame.add(label); //Ajouter label à frame
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JButton button = new JButton("Retour");//créer une instance de JButton  
+		button.setBounds(10,10,100, 40);//x axes, y axes, largeur, hauteur  
+		button.setBackground(Color.BLACK); //Définir la couleur de fond  
+		button.setForeground(Color.WHITE);
+		button.setFont(new Font("Rockwell Nova", Font.PLAIN, 15));
+		frame.add(button);//Ajouter bouton dans JFrame  
+				
+		
         
-		JButton b = new JButton("Se connecter");//créer une instance de JButton  
-		b.setBounds(300,300, 200, 70);//x axes, y axes, largeur, hauteur  
-		b.setBackground(Color.BLACK); //Définir la couleur de fond  
-		b.setForeground(Color.WHITE);
-		b.setFont(new Font("Rockwell Nova", Font.PLAIN, 15));
-		f.add(b);//Ajouter bouton dans JFrame  
+		JButton button2 = new JButton("Se connecter");//créer une instance de JButton  
+		button2.setBounds(300,300, 200, 70);//x axes, y axes, largeur, hauteur  
+		button2.setBackground(Color.BLACK); //Définir la couleur de fond  
+		button2.setForeground(Color.WHITE);
+		button2.setFont(new Font("Rockwell Nova", Font.ITALIC, 15));
+		frame.add(button2);//Ajouter bouton dans JFrame  
+		
+		JPanel panel = new JPanel();
+		JTextField field = new JTextField(" Identifiant : ", 20);
+		field.setBounds(250,100, 300, 50);
+		field.setFont(new Font("Rockwell Nova", Font.PLAIN, 15));
+		frame.add(field);
+		field.setVisible(true);
+		
+		field.addActionListener(new ActionListener()
+        {
+               public void actionPerformed(ActionEvent e)
+               {
+                     String input = field.getText();
+                     label.setText(input); 
+               }
+        });
+		
+		panel.add(label); 
+        frame.add(panel);
+        
+        JPanel panel2 = new JPanel();
+		JTextField field2 = new JTextField(" Mot de passe : ", 20);
+		field2.setBounds(250,200, 300, 50);
+		field2.setFont(new Font("Rockwell Nova", Font.PLAIN, 15));
+		frame.add(field2);
+		field2.setVisible(true);
+	    
+
+		field2.addActionListener(new ActionListener()
+        {
+               public void actionPerformed(ActionEvent e)
+               {
+                     String input = field2.getText();
+                     label.setText(input); 
+               }
+        });
+		
+		panel2.add(label); 
+        frame.add(panel2);
+		
+		/** Button Reaction **/
+		/* panel.add(button);
+		 
+         button.addActionListener(new ActionListener()
+         {
+                 public void actionPerformed(ActionEvent e)
+                 {
+                        String input = field.getText();
+                        label.setText(input);
+                 }
+         });
+         */
+         
+         frame.setVisible(true);//Rendre le frame visible
 	}
 }
